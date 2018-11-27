53d4f70f28a0506e60d1fb9b2aa633fe1f49570f
tjgus95 <43589861+tjgus95@users.noreply.github.com>
2018-11-27 10:35:57 -0500

    Update initMode.sh

--- BASH2-master/initMode.sh
+++ BASH2-master/initMode.sh
@@ -2,29 +2,29 @@
 
 #COUNTER=0
 
-#while [ $COUNTER -lt 4 ] # while the counter is less than 4
+#while [ $COUNTER -lt 4 ] 
 
-pins='0 1 2 3 4' #for loop takes a list of items
-for pins in $pins #for each of the items in the list do the following
+pins='0 1 2 3 4' 
+for pins in $pins 
 do
 		#echo $COUNTER # do this
-		gpio mode $pins out; #here  we set it to out
-		gpio write $pins 0; #here  0 is the value we set it to
+		gpio mode $pins out; 
+		gpio write $pins 0; 
 		
 		
-		#((COUNTER++)) # increment counter
+		#((COUNTER++)) 
 done
 
 
-pins2='5' #for loop takes a list of items, this is the BUTTON
-for pins2 in $pins2 #for each of the items in the list do the following
+pins2='5' 
+for pins2 in $pins2 
 do
 		#echo $COUNTER # do this
-		gpio mode $pins2 in; #here  we set it to in
-		gpio mode $pins2 up; #pull up		
-		gpio read $pins2 ; #here  we read the button
+		gpio mode $pins2 in; 
+		gpio mode $pins2 up; 	
+		gpio read $pins2 ; 
 		
-		#((COUNTER++)) # increment counter
+		#((COUNTER++)) 
 done
 
 #echo "done"
